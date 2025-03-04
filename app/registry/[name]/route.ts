3b3d19b0b44dbb1e8f15315af27b0f46a4eadd08
24180 <2418013844@qq.com>
2025-03-04 18:12:37 +0800

    build:change the type of time-picker

--- app/registry/[name]/route.ts
+++ app/registry/[name]/route.ts
@@ -6,10 +6,10 @@ import { registryItemSchema } from "shadcn/registry"
 // This route shows an example for serving a component using a route handler.
 export async function GET(
   request: Request,
-  { params }: { params: { name: string } }
+  context: { params: Promise<{ name: string }> },
 ) {
   try {
-    const { name } = params
+    const { name } = await context.params
     // Cache the registry import
     const registryData = await import("@/registry.json")
     const registry = registryData.default
