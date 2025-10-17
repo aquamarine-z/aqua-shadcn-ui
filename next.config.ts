960f170f5de45f2ed4ce1a0266f740bba6a4d776
24180 <2418013844@qq.com>
2025-10-17 21:13:02 +0800

    feat:add mdx support

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,21 @@
-import type { NextConfig } from "next";
+import createMDX from '@next/mdx'
+import type {NextConfig} from 'next'
+// 1️⃣ 包装 MDX 插件
+const withMDX = createMDX({
+    options: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+    },
+    extension: /\.(md|mdx)$/,
+})
 
+// 2️⃣ 主配置
 const nextConfig: NextConfig = {
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+    outputFileTracingIncludes: {
+        registry: ['./registry/**/*'],
+    },
+}
 
-  outputFileTracingIncludes: {
-    registry: ["./registry/**/*"],
-  },
-  /* config options here */
-};
-
-export default nextConfig;
+// 3️⃣ 导出
+export default withMDX(nextConfig)
